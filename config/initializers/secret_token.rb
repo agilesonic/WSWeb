e79a55675a80556b2f81058a8740de1df05b9b3f
agilesonic <agilesonic@gmail.com>
2012-12-29 09:38:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WSWeb::Application.config.secret_token = '07340d6e45ca94c6756de681113097afb5e1e7e7f6d89671492a40aa81897b06dd5c971d0c8ca7f98457dd52b7e435a426b2abe2614c11de83d70f209f28ca18'
